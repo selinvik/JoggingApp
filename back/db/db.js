162a7ce28fd0a3b58a0190d19525eb721a34975e
VincentoLaw <ergovincentolaw@gmail.com>
2019-10-30 18:16:38 +0200

    Restore password

--- back/db/db.js
+++ back/db/db.js
@@ -2,7 +2,7 @@ const session = require("express-session");
 const Sequelize = require('sequelize');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sequelize = new Sequelize('JogTracker', 'postgres', 'iIsK7s2ooxD4R1R4janZ', {
+const sequelize = new Sequelize('JogTracker', 'postgres', '3345', {
   host: 'localhost',
   dialect: 'postgres'
 });
