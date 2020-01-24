cc9a9e2008a8e981219af4db0b119b8e6a11b758
Viktor Selin <selinvictor1993@mail.ru>
2020-01-24 14:59:32 +0200

    Add todo to improve error handling

--- back/middlewares/error.ts
+++ back/middlewares/error.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from 'express';
 
-const error: RequestHandler = async (err, req, res, next) => {
-  console.error(err.stack);
+const error: RequestHandler = async (req, res, next) => {
+  //todo where is error here
+  //console.error(err.stack);
   res.status(500);
 }
 
